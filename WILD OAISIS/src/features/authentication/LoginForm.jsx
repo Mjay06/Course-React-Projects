43072dce95eddc557258a637dd7fbe7d46f70233
Ogunyemi  Oluwasemiloore <108068525+Mjay06@users.noreply.github.com>
2024-09-18 13:41:03 +0100

    testing

--- WILD OAISIS/src/features/authentication/LoginForm.jsx
+++ WILD OAISIS/src/features/authentication/LoginForm.jsx
@@ -32,7 +32,7 @@ function LoginForm() {
         />
       </FormRow>
       <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+        <Button size="large">login</Button>
       </FormRow>
     </Form>
   );
